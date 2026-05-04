0c9f053a88cd28977b3f3b95809f75e05e042729
EolMolina <eoleguizamonmo@unadvirtual.edu.co>
2026-05-04 17:11:07 -0500

    Realice correccciones con el tema de tkinter, añadi las 10 simulaciones, realice cambios en el tema de reserva y agregue otra clase para el registro del log

---
+++ Reserva.py
@@ -0,0 +1,9 @@
+class Reserva:
+    def __init__(self, cliente, servicio, duracion):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duracion = duracion
+        self.estado = "pendiente"
+
+    def confirmar(self):
+        if 
